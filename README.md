4fd574c4f9285a9bbc5a05c398505d7b3419e5f3
Luke Warlow <luke@warlow.dev>
2024-02-23 14:10:09 +0000

    Update to new `closedby` enum attribute design.

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 Proposal: https://github.com/whatwg/html/issues/9373
 
 Code based on https://github.com/keithamus/invoker-buttons-proposal
+
+This adds a new closedBy property and closedby attribute to the HTMLDialogElement.
+
+| Keyword        | Description |
+|----------------|---------------|
+| 'none'         | No automatic closing. |
+| 'closerequest' | Close watchers trigger close |
+| 'any'          | Light dismiss and close watchers trigger close |
+
+The default is an 'auto' behaviour with no matching keyword. This maintains existing behaviour, 'closerequest' for modal dialogs, else 'none'.
--- index.js
+++ index.js
@@ -2,24 +2,27 @@ export function isSupported() {
   return (
     typeof HTMLDialogElement !== "undefined" &&
     typeof HTMLDialogElement.prototype === "object" &&
-    "lightdismiss" in HTMLDialogElement.prototype
+    "closedBy" in HTMLDialogElement.prototype
   );
 }
 
-function handleDialogClick(event) {
+/** @type {WeakMap<Document, Set<HTMLDialogElement>>} */
+const anyDialogList = new WeakMap();
+function lightDismissOpenDialogs(event) {
   if (!event.isTrusted) return;
-  if (event.defaultPrevented) return;
-  const relatedTarget = event.target;
-
-  if (!relatedTarget.hasAttribute("lightdismiss")) {
-    return relatedTarget.removeEventListener('click', handleDialogClick);
-  }
-  if (event.type === "click") {
-    const rect = relatedTarget.getBoundingClientRect();
-    const isInDialog = (rect.top <= event.clientY && event.clientY <= rect.top + rect.height &&
-        rect.left <= event.clientX && event.clientX <= rect.left + rect.width);
-    if (!isInDialog) {
-      relatedTarget.close();
+  // Composed path allows us to find the target within shadowroots
+  const target = event.composedPath()[0];
+  if (!target) return;
+  const document = target.ownerDocument;
+  if (anyDialogList.has(document)) {
+    for (const dialog of Array.from(anyDialogList.get(document)).reverse()) {
+      const rect = dialog.getBoundingClientRect();
+      const isInDialog = (rect.top <= event.clientY && event.clientY <= rect.top + rect.height &&
+          rect.left <= event.clientX && event.clientX <= rect.left + rect.width);
+      if (isInDialog) {
+        break;
+      }
+      dialog.close();
     }
   }
 }
@@ -34,14 +37,72 @@ function observeShadowRoots(ElementClass, callback) {
 }
 
 export function apply() {
+  const show = HTMLDialogElement.prototype.show;
+
+  HTMLDialogElement.prototype.show = function () {
+    show.call(this, ...arguments);
+    const closedby = this.closedBy;
+    if (['any', 'closerequest'].includes(closedby)) {
+      this.__closeWatcher = new CloseWatcher();
+      this.__closeWatcher.onclose = () => {
+        this.close();
+      };
+
+      if (closedby === 'any') {
+        const document = this.ownerDocument;
+        if (!anyDialogList.has(document)) {
+          anyDialogList.set(document, new Set());
+        }
+        anyDialogList.get(document).add(this);
+      }
+    }
+  }
+  const showModal = HTMLDialogElement.prototype.showModal;
+  HTMLDialogElement.prototype.showModal = function () {
+    showModal.call(this, ...arguments);
+    const closedby = this.closedBy;
+    if (closedby === 'any') {
+      const document = this.ownerDocument;
+      if (!anyDialogList.has(document)) {
+        anyDialogList.set(document, new Set());
+      }
+      anyDialogList.get(document).add(this);
+    } else if (closedby === 'none') {
+      // TODO: Polyfill this, tricky due to close watcher anti-abuse mechanisms
+    }
+  }
+  const close = HTMLDialogElement.prototype.close;
+  HTMLDialogElement.prototype.close = function () {
+    close.call(this, ...arguments);
+    this.__closeWatcher?.dispose?.();
+    const document = this.ownerDocument;
+    if (anyDialogList.has(document)) {
+      anyDialogList.get(document).delete(this);
+    }
+  }
+
+  Object.defineProperty(HTMLDialogElement.prototype, "closedBy", {
+    get() {
+      const val = this.getAttribute('closedby');
+      if (["none", "any", "closerequest"].includes(val))
+        return val;
+      return "";
+    },
+    set(newValue) {
+      if (["none", "any", "closerequest"].includes(newValue))
+        this.setAttribute("closedby", newValue);
+      else this.removeAttribute("closedby");
+    },
+    enumerable: true,
+    configurable: true,
+  });
+
   const observer = new MutationObserver((mutations) => {
     for (const mutation of mutations) {
-      if (mutation.type === "attributes" && mutation.attributeName === "lightdismiss" && mutation.target.tagName === "DIALOG") {
-        if (mutation.target.hasAttribute("lightdismiss")) {
-          mutation.target.addEventListener("click", handleDialogClick);
-        } else {
-          mutation.target.removeEventListener("click", handleDialogClick);
-        }
+      if (mutation.type === "attributes" && mutation.attributeName === "closedby" && mutation.target.tagName === "DIALOG") {
+        // TODO: Check what the expected behaviour here is. Closing is just the easiest.
+        if (mutation.oldValue !== mutation.target.getAttribute('closedby'))
+            mutation.target.close();
       }
     }
   });
@@ -49,7 +110,7 @@ export function apply() {
     childList: true,
     subtree: true,
     attributes: true,
-    attributeFilter: ["lightdismiss"],
+    attributeFilter: ["closedby"],
   };
   observer.observe(document, observerOptions);
 
@@ -57,7 +118,5 @@ export function apply() {
     observer.observe(shadow, observerOptions);
   });
 
-  for (const dialog of document.querySelectorAll("dialog[lightdismiss]")) {
-    dialog.addEventListener("click", handleDialogClick);
-  }
+  document.addEventListener("pointerdown", lightDismissOpenDialogs);
 }
